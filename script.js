6c6e8e965886dc1c25a20ac57d2ee8a0fa355586
@amritmohan <amritmohan2284@gmail.com>
2025-02-24 00:59:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function updateTimer() {
+	let timer = document.getElementById("timer");
+	let date = new Date();
+	timer.innerText = date.toLocaleString();
+}
+
+setInterval(updateTimer, 1000);
+updateTimer();
